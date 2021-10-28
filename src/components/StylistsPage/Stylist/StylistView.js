47a1aeed65af2e3a475619b8ef2481a48314bb7c
Kevin Camacho <kpcamacho@hotmail.com>
2021-10-28 19:56:12 -0400

    added facebook, instagram logos and css classes, added all stylist instagram links

--- src/components/StylistsPage/Stylist/StylistView.js
+++ src/components/StylistsPage/Stylist/StylistView.js
@@ -3,6 +3,8 @@ import './StylistView.scss';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import facebookIcon from '../../../images/socials/fb_logo.png';
+import instaIcon from '../../../images/socials/insta_logo.png';
 
 const Stylist = ({ stylist, index }) => {
     const isEvenStylist = index % 2 === 0;
@@ -10,7 +12,15 @@ const Stylist = ({ stylist, index }) => {
         <Row className="stylist-view">
             <Col xs={{ span: 12, order: 'last' }} md={{ span: 6, order: isEvenStylist ? 'first' : 'last' }}>
                 <Row>
-                    <div className="person-name">{stylist.name}</div>
+                    <Col>
+                        <div className="person-name">{stylist.name}</div>
+                    </Col>
+                    <Col xs="auto">
+                        <div className="flexbox-row social-logo-container">
+                            {stylist.facebook && <img className="facebook-logo" src={facebookIcon} alt="facebook" onClick={() => window.open(stylist.facebook)} />}
+                            {stylist.instagram && <img style={{ marginLeft: '17px' }} className="insta-logo" src={instaIcon} alt="instagram" onClick={() => window.open(stylist.instagram)} />}
+                        </div>
+                    </Col>
                     <div style={{ marginTop: '17px' }}>{stylist.flavorText}</div>
                 </Row>
             </Col>
