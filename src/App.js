6f4c4eb7741c310c20af58d5e04f542985c0ceaf
Kevin Camacho <kpcamacho@hotmail.com>
2022-05-18 23:51:49 -0400

    update of all packages, react-router v6 migration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Switch, Route, useHistory } from 'react-router';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ServicesPage from './components/ServicesPage/ServicesPage';
 import './App.scss';
@@ -11,36 +11,31 @@ import StylistsPage from './components/StylistsPage/StylistsPage';
 import ContactUsPage from './components/ContactUsPage/ContactUsPage';
 
 const App = () => {
-    const history = useHistory();
-
-    const onBrandClick = () => {
-        history.push('/');
-        console.log('ayyo');
-    }
+    const navigate = useNavigate();
 
     return (
         <div id="mainContainer">
             <Navbar expand="md" bg="dark" variant="dark" sticky="top">
                 <Container fluid>
-                    <Navbar.Brand id="randco_navbarBrand" onClick={onBrandClick}>Raquel & Company</Navbar.Brand>
+                    <Navbar.Brand id="randco_navbarBrand" onClick={() => navigate('/')}>Raquel & Company</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbar" />
                     <Navbar.Collapse id="navbar">
                         <Nav className="justify-content-end" style={{ width: "100%" }}>
-                            <Nav.Link href="/services" eventKey="Services">Services</Nav.Link>
-                            <Nav.Link href="/about-us" eventKey="AboutUs">About Us</Nav.Link>
-                            <Nav.Link href="/meet-the-stylists" eventKey="Stylists">Stylists</Nav.Link>
-                            <Nav.Link href="/contact-us" eventKey="ContactUs">Contact Us</Nav.Link>
+                            <Nav.Link eventKey="Services" onClick={() => navigate('services')}>Services</Nav.Link>
+                            <Nav.Link eventKey="AboutUs" onClick={() => navigate('about-us')}>About Us</Nav.Link>
+                            <Nav.Link eventKey="Stylists" onClick={() => navigate('/meet-the-stylists')}>Stylists</Nav.Link>
+                            <Nav.Link eventKey="ContactUs" onClick={() => navigate('/contact-us')}>Contact Us</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route exact path='/services' component={ServicesPage} />
-                <Route exact path='/about-us' component={AboutUsPage} />
-                <Route exact path='/meet-the-stylists' component={StylistsPage} />
-                <Route exact path='/contact-us' component={ContactUsPage} />
-            </Switch>
+            <Routes>
+                <Route exact path='/' element={<HomePage />} />
+                <Route exact path='/services' element={<ServicesPage />} />
+                <Route exact path='/about-us' element={<AboutUsPage />} />
+                <Route exact path='/meet-the-stylists' element={<StylistsPage />} />
+                <Route exact path='/contact-us' element={<ContactUsPage />} />
+            </Routes>
         </div>
     )
 };
