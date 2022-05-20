9d17cbf45b1111286e2937ef0cc769e9a6e7804f
Kevin Camacho <kpcamacho@hotmail.com>
2022-05-19 20:42:03 -0400

    removed some old comments

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ const App = () => {
                     <Navbar.Toggle aria-controls="navbar" />
                     <Navbar.Collapse id="navbar">
                         <Nav className="justify-content-end" style={{ width: "100%" }} onSelect={selectedPath => navigate(selectedPath)}>
-                            <Nav.Link eventKey="/services" /* onClick={() => navigate('services')} */>Services</Nav.Link>
-                            <Nav.Link eventKey="/about-us" /* onClick={() => navigate('about-us')} */>About Us</Nav.Link>
-                            <Nav.Link eventKey="/meet-the-stylists" /* onClick={() => navigate('/meet-the-stylists')} */>Stylists</Nav.Link>
-                            <Nav.Link eventKey="/contact-us" /* onClick={() => navigate('/contact-us')} */>Contact Us</Nav.Link>
+                            <Nav.Link eventKey="/about-us">About Us</Nav.Link>
+                            <Nav.Link eventKey="/services">Services</Nav.Link>
+                            <Nav.Link eventKey="/meet-the-stylists">Stylists</Nav.Link>
+                            <Nav.Link eventKey="/contact-us">Contact Us</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
