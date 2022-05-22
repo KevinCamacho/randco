aca6c2638d0c08755abfa1beed8d05a85fd78444
Kevin Camacho <kpcamacho@hotmail.com>
2022-05-22 13:11:45 -0400

    products json and image in products page

--- src/components/ProductsPage/ProductsPage.js
+++ src/components/ProductsPage/ProductsPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './ProductsPage.scss';
 import Container from 'react-bootstrap/Container';
+import productsData from '../../data/products.json';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const ProductsPage = () => {
+    const mainImage = require(`../../images/${productsData.heroImage}`);
     return (
         <Container fluid="md" className="route-root" style={{ textAlign: 'center' }}>
-            <div>hi</div>
+            <LazyLoadImage id="productsPage_mainImage" src={mainImage} effect="blur" />
         </Container>
     )
 };
