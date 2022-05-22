87ca3bd0ddbb2ed19a1aa8b79ed2f0d3d2fd8ef6
Kevin Camacho <kpcamacho@hotmail.com>
2022-05-22 12:50:53 -0400

    carousel can show different images depending on screen size

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,15 +5,18 @@ import Carousel from 'react-bootstrap/Carousel';
 import { useNavigate } from 'react-router-dom';
 import carouselData from '../data/carousel.json';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { useMediaQuery } from 'react-responsive';
 
 const HomePage = () => {
     const navigate = useNavigate();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     return (
         <Container fluid="md" className="route-root" style={{ textAlign: 'center' }}>
             <Carousel pause="hover">
                 {carouselData.map((carouselItem) => {
-                    const carouselImage = require(`../images/${carouselItem.image}`);
+                    const imageName = isMobile ? carouselItem.mobileImage : carouselItem.desktopImage;
+                    const carouselImage = require(`../images/${imageName}`);
                     return (
                         <Carousel.Item className="cursor-pointer" onClick={() => navigate(carouselItem.navigate)}>
                             <LazyLoadImage className="carousel-image" src={carouselImage} alt={carouselItem.altText} effect="blur" />
