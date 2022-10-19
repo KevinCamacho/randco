76a02bdc5a5676abd3529c2118d4aba758a9105d
Kevin Camacho <kpcamacho@hotmail.com>
2022-10-19 19:51:03 -0400

    added left and right border to stylist text on mobile view

--- src/components/StylistsPage/Stylist/StylistView.js
+++ src/components/StylistsPage/Stylist/StylistView.js
@@ -14,18 +14,22 @@ const Stylist = ({ stylist, index }) => {
     return (
         <Row className="stylist-view">
             <Col xs={{ span: 12, order: 'last' }} md={{ span: 6, order: isEvenStylist ? 'first' : 'last' }}>
-                <Row>
-                    <Col>
-                        <div className="person-name stylist-name">{stylist.name}</div>
-                    </Col>
-                    <Col xs="auto">
-                        <div className="flexbox-row social-logo-container">
-                            {stylist.facebook && <img className="facebook-logo" src={facebookIcon} alt="facebook" onClick={() => window.open(stylist.facebook)} />}
-                            {stylist.instagram && <img style={{ marginLeft: '17px' }} className="insta-logo" src={instaIcon} alt="instagram" onClick={() => window.open(stylist.instagram)} />}
+                <Row className="stylist-name-and-bio">
+                    <div>
+                        <Row>
+                            <Col>
+                                <div className="person-name stylist-name">{stylist.name}</div>
+                            </Col>
+                            <Col xs="auto">
+                                <div className="flexbox-row social-logo-container">
+                                    {stylist.facebook && <img className="facebook-logo" src={facebookIcon} alt="facebook" onClick={() => window.open(stylist.facebook)} />}
+                                    {stylist.instagram && <img style={{ marginLeft: '17px' }} className="insta-logo" src={instaIcon} alt="instagram" onClick={() => window.open(stylist.instagram)} />}
+                                </div>
+                            </Col>
+                        </Row>
+                        <div className="flavor-text">
+                            {stylist.flavorText.split('\n').map(text => <div>{text}</div>)}
                         </div>
-                    </Col>
-                    <div className="flavor-text">
-                        {stylist.flavorText.split('\n').map(text => <div>{text}</div>)}
                     </div>
                 </Row>
             </Col>
