a472161e327c5abf5b6f63491d093c195cd16ca3
Kevin Camacho <kpcamacho@hotmail.com>
2022-10-19 22:49:50 -0400

    rework of services page

--- src/components/ServicesPage/ServicesPage.js
+++ src/components/ServicesPage/ServicesPage.js
@@ -8,34 +8,79 @@ import data from '../../data/services.json';
 const ServicesPage = () => {
     return (
         <Container fluid="md" className="route-root" style={{ textAlign: 'center' }}>
-            <Row>
-                <div id="services-header">{data.header}</div>
-            </Row>
-            <Row>
-                <div className="price-disclaimer">{data.priceDisclaimer}</div>
-            </Row>
-            {data.serviceList.map((serviceCategory, index) =>
-                <div key={serviceCategory.title}>
-                    <div className="horizontal-divider-hidden"></div>
-                    <Row className="services-container">
-                        <div>
-                            <div className="service-category-title">{serviceCategory.title}</div>
-                            <div className="service-list">
-                                {serviceCategory.items.map(service =>
-                                    <Row key={service.serviceName} className="service-line-item">
-                                        <Col className="service-title">{service.serviceName}</Col>
-                                        <Col className="service-price">{service.price}</Col>
-                                    </Row>
-                                )}
-                            </div>
-                            {serviceCategory.sectionFooter && (
-                                <div className="service-footer">***{serviceCategory.sectionFooter}***</div>
+            {/* <Row className="align-items-center" style={{ textAlign: 'right' }}>
+                <Col>
+                    {data.serviceList.map((serviceCategory, index) =>
+                        <div key={`ServiceList${serviceCategory.title}`}>
+                            {index > 0 && (
+                                <Row className="service-list-empty-row">
+                                    <Col xs={4}></Col>
+                                    <Col xs="auto"><div className="vertical-divider"></div></Col>
+                                    <Col xs={4}></Col>
+                                </Row>
+                            )}
+                            <Row className="justify-content-center">
+                                <Col xs={4}><div className="service-category-title service-name">{serviceCategory.title}</div></Col>
+                                <Col xs="auto"><div className="vertical-divider"></div></Col>
+                                <Col xs={4}><div className="service-price"></div></Col>
+                            </Row>
+                            {serviceCategory.items.map(service =>
+                                <Row className="justify-content-center service-list" key={`ServiceListLineItem${service.name}`}>
+                                    <Col xs={4}><div className="service-name">{service.name}</div></Col>
+                                    <Col xs="auto"><div className="vertical-divider"></div></Col>
+                                    <Col xs={4}><div className="service-price">${service.price}</div></Col>
+                                </Row>
                             )}
                         </div>
-                    </Row>
+                    )}
+                </Col>
+                <Col xs="auto">
+                    <div className="side-text">{"P R I C E"}</div>
+                    <div style={{ marginTop: '34px' }} className="side-text">{"L I S T"}</div>
+                </Col>
+            </Row> */}
+
+
+            <div className="align-items-center" style={{ display: 'flex', flexWrap: 'nowrap' }}>
+                <div style={{ flex: '1 1 auto', display: 'flex', flexDirection: 'column' }}>
+                    {data.serviceList.map((serviceCategory, index) =>
+                        <div key={`ServiceList${serviceCategory.title}`} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            {index > 0 && (
+                                <div style={{ display: 'flex', flexWrap: 'nowrap' }} className="service-list-empty-row">
+                                    <div style={{ width: '180px' }}></div>
+                                    <div style={{ height: 'inherit' }} className="vertical-divider"></div>
+                                    <div style={{ width: '75px' }}></div>
+                                </div>
+                            )}
+                            <div style={{ display: 'flex', flexWrap: 'nowrap' }}>
+                                <div style={{ width: '180px' }} className="service-category-title service-name">{serviceCategory.title}</div>
+                                <div style={{ height: 'inherit', marginLeft: '10px', marginRight: '10px' }} className="vertical-divider"></div>
+                                <div style={{ width: '75px' }} className="service-price"></div>
+                            </div>
+                            {serviceCategory.items.map(service =>
+                                <div style={{ display: 'flex', flexWrap: 'nowrap' }} className="service-list" key={`ServiceListLineItem${service.name}`}>
+                                    <div style={{ width: '180px' }} className="service-name">{service.name}</div>
+                                    <div style={{ height: 'inherit', marginLeft: '10px', marginRight: '10px' }} className="vertical-divider"></div>
+                                    <div style={{ width: '75px' }} className="service-price">${service.price}</div>
+                                </div>
+                            )}
+                        </div>
+                    )}
                 </div>
-            )}
-        </Container>
+                <div style={{ flex: '0 0 content' }}>
+                    <div className="side-text">{"P R I C E"}</div>
+                    <div style={{ marginTop: '34px' }} className="side-text">{"L I S T"}</div>
+                </div>
+            </div>
+
+
+            <Row style={{ marginTop: '68px' }}>
+                <div className="service-page-footer">{data.footer}</div>
+            </Row>
+            <Row style={{ marginTop: '17px' }}>
+                <div className="service-page-price-disclaimer">{data.priceDisclaimer}</div>
+            </Row>
+        </Container >
     )
 };
 
