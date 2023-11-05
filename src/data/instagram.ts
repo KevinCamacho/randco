65a0afc4c6d68967890e616794c7f23ab79b9ba4
Kevin Camacho <kpcamacho@hotmail.com>
2023-11-05 12:56:55 -0500

    updated instagram URLs

--- src/data/instagram.ts
+++ src/data/instagram.ts
@@ -1,5 +1,5 @@
 export const instagram: string[] = [
+    'https://www.instagram.com/p/CzOSuZjrYuZ/',
     'https://www.instagram.com/p/Ct-WLNCMa77/',
-    'https://www.instagram.com/p/Csr1hK8LO8A/',
-    'https://www.instagram.com/p/CrzFjRkrQ_l/'
+    'https://www.instagram.com/p/Csr1hK8LO8A/'
 ];
